254ae43ca1939c2b7d5da5502f55f25e3849b080
devfd <franz.dumas@gmail.com>
2016-05-18 22:38:15 +0200

    improve parsing of geocoding results

--- android/src/main/java/com/devfd/RNGeocoder/RNGeocoderModule.java
+++ android/src/main/java/com/devfd/RNGeocoder/RNGeocoderModule.java
@@ -3,85 +3,112 @@ package com.devfd.RNGeocoder;
 import android.location.Address;
 import android.location.Geocoder;
 
-import com.facebook.react.bridge.Callback;
-import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
 import com.facebook.react.bridge.WritableArray;
 import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.WritableNativeArray;
 import com.facebook.react.bridge.WritableNativeMap;
 
-import org.json.JSONArray;
-
 import java.io.IOException;
 import java.util.List;
 
 public class RNGeocoderModule extends ReactContextBaseJavaModule {
 
-  public RNGeocoderModule(ReactApplicationContext reactContext) {
-    super(reactContext);
-  }
+    private Geocoder geocoder;
 
-  @Override
-  public String getName() {
-    return "RNGeocoder";
-  }
-
-  @ReactMethod
-  public void geocodeAddress(String addressName, Callback errorCallback, Callback successCallback) {
-    Geocoder geocoder = new Geocoder(getReactApplicationContext());
-    try {
-      List<Address> addresses = geocoder.getFromLocationName(addressName, 20);
-      successCallback.invoke(transform(addresses));
-    }
-    catch (IOException e) {
-      errorCallback.invoke(e.getMessage());
-    }
-  }
-
-  @ReactMethod
-  public void reverseGeocodeLocation(ReadableMap position, Callback errorCallback, Callback successCallback) {
-    Geocoder geocoder = new Geocoder(getReactApplicationContext());
-
-    try {
-      List<Address> addresses = geocoder.getFromLocation(position.getDouble("latitude"), position.getDouble("longitude"), 20);
-      successCallback.invoke(transform(addresses));
-    }
-    catch (IOException e) {
-      errorCallback.invoke(e.getMessage());
+    public RNGeocoderModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        geocoder = new Geocoder(reactContext.getApplicationContext());
     }
 
-  }
-
-  WritableArray transform(List<Address> addresses) {
-    WritableArray results = new WritableNativeArray();
-
-    for (Address address: addresses) {
-      WritableMap result = new WritableNativeMap();
-
-      WritableMap position = new WritableNativeMap();
-      position.putDouble("lat", address.getLatitude());
-      position.putDouble("lng", address.getLongitude());
-      result.putMap("position", position);
-
-      result.putString("locality", address.getLocality());
-      result.putString("adminArea", address.getAdminArea());
-      result.putString("country", address.getCountryName());
-      result.putString("countryCode", address.getCountryCode());
-      result.putString("locale", address.getLocale().toString());
-      result.putString("postalCode", address.getPostalCode());
-      result.putString("subAdminArea", address.getSubAdminArea());
-      result.putString("subLocality", address.getSubLocality());
-      result.putString("subThoroughfare", address.getSubThoroughfare());
-      result.putString("thoroughfare", address.getThoroughfare());
-      results.pushMap(result);
+    @Override
+    public String getName() {
+        return "RNGeocoder";
     }
 
-    return results;
-  }
+    @ReactMethod
+    public void geocodeAddress(String addressName, Promise promise) {
+        if (!geocoder.isPresent()) {
+          promise.reject("-1", "Geocoder not available for this platform");
+          return;
+        }
+
+        try {
+            List<Address> addresses = geocoder.getFromLocationName(addressName, 20);
+            promise.resolve(transform(addresses));
+        }
+
+        catch (IOException e) {
+            promise.reject(e);
+        }
+    }
+
+    @ReactMethod
+    public void geocodePosition(ReadableMap position, Promise promise) {
+        if (!geocoder.isPresent()) {
+            promise.reject("-1", "Geocoder not available for this platform");
+            return;
+        }
+
+        try {
+            List<Address> addresses = geocoder.getFromLocation(position.getDouble("lat"), position.getDouble("lng"), 20);
+            promise.resolve(transform(addresses));
+        }
+        catch (IOException e) {
+            promise.reject(e);
+        }
+    }
+
+    WritableArray transform(List<Address> addresses) {
+        WritableArray results = new WritableNativeArray();
+
+        for (Address address: addresses) {
+            WritableMap result = new WritableNativeMap();
+
+            WritableMap position = new WritableNativeMap();
+            position.putDouble("lat", address.getLatitude());
+            position.putDouble("lng", address.getLongitude());
+            result.putMap("position", position);
+
+            if (!address.getFeatureName().equals(address.getSubThoroughfare()) &&
+                    !address.getFeatureName().equals(address.getThoroughfare()) &&
+                    !address.getFeatureName().equals(address.getLocality())) {
+
+                result.putString("feature", address.getFeatureName());
+            }
+            else {
+                result.putString("feature", null);
+            }
+
+            result.putString("locality", address.getLocality());
+            result.putString("adminArea", address.getAdminArea());
+            result.putString("country", address.getCountryName());
+            result.putString("countryCode", address.getCountryCode());
+            result.putString("locale", address.getLocale().toString());
+            result.putString("postalCode", address.getPostalCode());
+            result.putString("subAdminArea", address.getSubAdminArea());
+            result.putString("subLocality", address.getSubLocality());
+            result.putString("streetNumber", address.getSubThoroughfare());
+            result.putString("streetName", address.getThoroughfare());
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i <= address.getMaxAddressLineIndex(); i++) {
+                if (i > 0) {
+                    sb.append(',');
+                }
+                sb.append(address.getAddressLine(i));
+            }
+
+            result.putString("formattedAddress", sb.toString());
+
+            results.pushMap(result);
+        }
+
+        return results;
+    }
 }
